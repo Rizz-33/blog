e53ad4642cec40fe85de368cbc120cefeaa33c8b
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-23 13:15:49 +0530

    feat: Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 export default function App() {
-  return <h1 className="text-3xl text-violet-600 font-extrabold">Blog.</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
