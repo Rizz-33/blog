088b6d1da66edab0f56ce960096bec00ab10f43d
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-27 16:59:03 +0530

    refactor: Onclick handle for GoogleAuth button

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -1,8 +1,22 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { Button } from "flowbite-react";
 import React from "react";
 import { AiFillGoogleCircle } from "react-icons/ai";
+import { app } from "../firebase";
 
 const GoogleAuth = () => {
+  const auth = getAuth(app);
+  const handleGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
+    try {
+      const resultFromGoogle = await signInWithPopup(auth, provider);
+      console.log(resultFromGoogle);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <Button
       type="button"
