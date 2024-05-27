1ba637294692d281fd49d6bcd1f55389045d5014
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-27 10:37:26 +0530

    refactor: Store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,9 @@
+// src/redux/store.js
 import { configureStore } from "@reduxjs/toolkit";
-import { useReducer } from "./user/userSlice";
+import userReducer from "./user/userSlice"; // Import the default export, which is the reducer
 
 export const store = configureStore({
   reducer: {
-    user: useReducer,
+    user: userReducer, // Use the imported userReducer
   },
 });
