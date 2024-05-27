bdc8246f1013fce6966a4c853510ef33d0ea9d3a
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-27 14:49:47 +0530

    refactor: Firebase installation and initialization

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "blog-web-app-30b0c.firebaseapp.com",
+  projectId: "blog-web-app-30b0c",
+  storageBucket: "blog-web-app-30b0c.appspot.com",
+  messagingSenderId: "983233322317",
+  appId: "1:983233322317:web:d66ff76a8112cbee0aeb04",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
