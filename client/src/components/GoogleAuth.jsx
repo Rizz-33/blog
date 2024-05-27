9e3a216dae2fd78aa228e9637fc5cd7330f2a73c
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-27 17:54:39 +0530

    refactor: Onclick handle for GoogleAuth button

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -2,16 +2,34 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { Button } from "flowbite-react";
 import React from "react";
 import { AiFillGoogleCircle } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { app } from "../firebase";
+import { signInSuccess } from "../redux/user/userSlice";
 
 const GoogleAuth = () => {
   const auth = getAuth(app);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogle = async () => {
     const provider = new GoogleAuthProvider();
     provider.setCustomParameters({ prompt: "select_account" });
     try {
       const resultFromGoogle = await signInWithPopup(auth, provider);
-      console.log(resultFromGoogle);
+      const res = await fetch("/api/auth/google", {
+        body: JSON.stringify({
+          tokenId: resultFromGoogle.user.uid,
+          displayName: resultFromGoogle.user.displayName,
+          email: resultFromGoogle.user.email,
+          photoURL: resultFromGoogle.user.photoURL,
+          emailVerified: resultFromGoogle.user.emailVerified,
+        }),
+      });
+      const data = await resultFromGoogle.json();
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
     } catch (error) {
       console.log(error.message);
     }
