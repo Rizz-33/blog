808eff981c7a432ebbbd17074e93a16b9709f153
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-29 13:09:06 +0530

    feat: Sidebar

--- client/src/components/dashboard/Sidebar.jsx
+++ client/src/components/dashboard/Sidebar.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { Sidebar } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { HiCog, HiLogout, HiTrash, HiUser } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const CustomSidebar = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <Sidebar>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item
+            active={tab === "settings"}
+            icon={HiCog}
+            labelColor="info"
+          >
+            Settings
+          </Sidebar.Item>
+          <Sidebar.Item
+            active={tab === "signout"}
+            icon={HiLogout}
+            labelColor="warning"
+          >
+            Sign Out
+          </Sidebar.Item>
+          <Sidebar.Item
+            active={tab === "delete"}
+            icon={HiTrash}
+            labelColor="danger"
+          >
+            Delete My Account
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
 };
 
-export default Sidebar;
+export default CustomSidebar;
