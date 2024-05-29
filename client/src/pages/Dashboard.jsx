bcd43d623e242de61bdf9586c490e1edd3130485
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-29 12:34:32 +0530

    refactor: Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    console.log(tabFromUrl);
+  }, [location.search]);
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Dashboard;
