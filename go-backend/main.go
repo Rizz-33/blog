307677034d9016335dc4c7657ca6cbf4b96c9cc4
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-04 11:22:22 +0530

    refactor: Main

--- go-backend/main.go
+++ go-backend/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/Rizz-33/blog/go-backend/controllers"
 	"github.com/Rizz-33/blog/go-backend/routes"
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -16,50 +17,50 @@ import (
 var database *mongo.Database
 
 func withCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
+        if r.Method == http.MethodOptions {
+            w.WriteHeader(http.StatusNoContent)
+            return
+        }
 
-		next.ServeHTTP(w, r)
-	})
+        next.ServeHTTP(w, r)
+    })
 }
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 
-	connectionString := os.Getenv("MONGO_CONNECTION_STRING")
-	if connectionString == "" {
-		log.Fatal("MongoDB connection string not provided")
-	}
+    connectionString := os.Getenv("MONGO_CONNECTION_STRING")
+    if connectionString == "" {
+        log.Fatal("MongoDB connection string not provided")
+    }
 
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(connectionString))
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
-	}
-	defer client.Disconnect(context.Background())
+    client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(connectionString))
+    if err != nil {
+        log.Fatal("Failed to connect to database:", err)
+    }
+    defer client.Disconnect(context.Background())
 
-	err = client.Ping(context.Background(), nil)
-	if err != nil {
-		log.Println("Failed to ping database:", err)
-	} else {
-		log.Println("Connected to database successfully!")
-	}
+    err = client.Ping(context.Background(), nil)
+    if err != nil {
+        log.Println("Failed to ping database:", err)
+    } else {
+        log.Println("Connected to database successfully!")
+    }
 
-	database = client.Database("test")
-	controllers.InitializeDatabase(database)
+    database = client.Database("test")
+    controllers.InitializeDatabase(database)
 
-	mux := http.NewServeMux()
-	routes.AuthRoutes(mux)
+    router := mux.NewRouter()
+    routes.UserRoutes(router)
 
-	log.Println("Starting server on :8000")
-	log.Fatal(http.ListenAndServe(":8000", withCORS(mux)))
+    log.Println("Starting server on :8000")
+    log.Fatal(http.ListenAndServe(":8000", withCORS(router)))
 }
