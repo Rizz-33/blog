a39e6cb725dcd1378e11707bea33f02c0f43ebac
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-04 11:00:07 +0530

    refactor: User routes with methods

--- go-backend/go.mod
+++ go-backend/go.mod
@@ -2,9 +2,11 @@ module github.com/Rizz-33/blog/go-backend
 
 go 1.22.2
 
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
--- go-backend/routes/userRoute.go
+++ go-backend/routes/userRoute.go
@@ -1,14 +1,13 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/Rizz-33/blog/go-backend/controllers"
+	"github.com/gorilla/mux"
 )
 
-func UserRoutes() {
-	http.HandleFunc("/api/user", controllers.GetUser)
-	http.HandleFunc("/api/user/create", controllers.CreateUser)
-	http.HandleFunc("/api/user/update", controllers.UpdateUser)
-	http.HandleFunc("/api/user/delete", controllers.DeleteUser)
+func UserRoutes(router *mux.Router) {
+    router.HandleFunc("/user", controllers.CreateUser).Methods("POST")
+    router.HandleFunc("/user", controllers.GetUser).Methods("GET")
+    router.HandleFunc("/user", controllers.UpdateUser).Methods("PUT")
+    router.HandleFunc("/user", controllers.DeleteUser).Methods("DELETE")
 }
