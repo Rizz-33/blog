be42615912d7d9028a60c3ae8e423ddaa40557e7
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-05 08:20:29 +0530

    refactor: User slice with update func - redux

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -23,9 +23,29 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    updateUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = { ...state.currentUser, ...action.payload };
+      state.loading = false;
+      state.error = null;
+    },
+    updateUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { signInFailure, signInStart, signInSuccess } = userSlice.actions;
+export const {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} = userSlice.actions;
 
 export default userSlice.reducer;
